b623737eb661878046d38d342e892b64e24b2463
MCForger <mrseanmcd@gmail.com>
2012-10-27 18:41:24 -0400

    Removed something from the VoteSQLCommand.

--- src/me/javoris767/votesql/commands/VoteSQLCommand.java
+++ src/me/javoris767/votesql/commands/VoteSQLCommand.java
@@ -37,7 +37,6 @@ public class VoteSQLCommand implements CommandExecutor
 			sender.sendMessage(ChatColor.YELLOW + "/votesql check <string> -"
 					+ ChatColor.BLUE
 					+ " Adds a string and 1 vote to the database.");
-			VoteSQLChat.broadcastVoteMessage();
 		}
 		else if (args.length == 1)
 		{
